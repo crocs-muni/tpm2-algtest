f128fd3ea48bd183b292ca2417f81a673f26edda
Simon Struk <simon.struk@gmail.com>
2018-12-05 05:25:25 +0100

    Measure TPM2_Create (all).

--- algtest/create.c
+++ algtest/create.c
@@ -1,6 +1,6 @@
 #include "util.h"
 
-static const int NUM_MEASUREMENTS = 100;
+static const int NUM_MEASUREMENTS = 10;
 
 TPMI_DH_OBJECT create_parent(TSS2_SYS_CONTEXT *sapi_context,
         TPMI_DH_OBJECT *parentHandle)
@@ -147,6 +147,153 @@ void measure_Create_RSA(TSS2_SYS_CONTEXT *sapi_context,
     fclose(out_all);
 }
 
+void measure_Create_ECC(TSS2_SYS_CONTEXT *sapi_context,
+        struct create_params *params, TPMI_DH_OBJECT parentHandle)
+{
+    puts("Measuring Create (ECC)...");
+
+    /* Create ECC template */
+    params->inPublic.publicArea.type = TPM2_ALG_ECC;
+    TPMU_PUBLIC_PARMS parameters = {
+        .eccDetail = {          // TPMS_ECC_PARMS
+            .symmetric = TPM2_ALG_NULL,
+            .scheme = TPM2_ALG_NULL,        // TPMT_ECC_SCHEME+
+            .kdf = TPM2_ALG_NULL
+        }
+    };
+    params->inPublic.publicArea.parameters = parameters;
+
+    FILE *out = open_csv("TPM2_Create_ECC.csv",
+            "curveID;duration_mean;error_codes", "w");
+    FILE *out_all = open_csv("TPM2_Create_ECC_all.csv",
+            "curveID;duration;return_code", "w");
+
+    for (TPM2_ECC_CURVE curve = 0x00; curve <= 0x0020; ++curve) {
+        params->inPublic.publicArea.parameters.eccDetail.curveID = curve;
+        char param_fields[7];
+        snprintf(param_fields, 7, "%04x", curve);
+        test_and_measure(sapi_context, param_fields, params, parentHandle,
+                out, out_all);
+    }
+
+    fclose(out);
+    fclose(out_all);
+}
+
+void measure_Create_SYMCIPHER(TSS2_SYS_CONTEXT *sapi_context,
+        struct create_params *params, TPMI_DH_OBJECT parentHandle)
+{
+    puts("Measuring Create (SYMCIPHER)...");
+
+    /* Create ECC template */
+    params->inPublic.publicArea.type = TPM2_ALG_SYMCIPHER;
+    TPMU_PUBLIC_PARMS parameters = {
+        .symDetail = {          // TPMS_SYMCIPHER_PARMS
+            .sym = {
+                .mode = TPM2_ALG_CFB,        // TPMU_SYM_MODE
+            }
+        }
+    };
+    params->inPublic.publicArea.parameters = parameters;
+
+    FILE *out = open_csv("TPM2_Create_SYMCIPHER.csv",
+            "algorithm;keyBits;duration_mean;error_codes", "w");
+    FILE *out_all = open_csv("TPM2_Create_SYMCIPHER_all.csv",
+            "algorithm;keyBits;duration;return_code", "w");
+
+    for (TPMI_ALG_SYM_OBJECT algorithm = TPM2_ALG_FIRST;
+            algorithm < TPM2_ALG_LAST; ++algorithm) {
+        params->inPublic.publicArea.parameters.symDetail.sym.algorithm
+            = algorithm;
+        for (int keyBits = 0; keyBits <= TPM2_MAX_SYM_KEY_BYTES * 8; keyBits += 32) {
+            params->inPublic.publicArea.parameters.symDetail.sym.keyBits.sym
+                = keyBits;
+            char param_fields[12];
+            snprintf(param_fields, 12, "%04x;%d", algorithm, keyBits);
+            test_and_measure(sapi_context, param_fields, params, parentHandle,
+                    out, out_all);
+        }
+    }
+    fclose(out);
+    fclose(out_all);
+}
+
+void measure_Create_KEYEDHASH(TSS2_SYS_CONTEXT *sapi_context,
+        struct create_params *params, TPMI_DH_OBJECT parentHandle)
+{
+    puts("Measuring CreatePrimary (KEYEDHASH)...");
+
+    /* KEYEDHASH cannot decrypt */
+    params->inPublic.publicArea.objectAttributes &= ~TPMA_OBJECT_DECRYPT;
+
+    TPMI_ALG_KEYEDHASH_SCHEME schemes[] = {
+        TPM2_ALG_HMAC,
+        TPM2_ALG_XOR,
+        TPM2_ALG_NULL
+    };
+
+    /* Create KEYEDHASH template */
+    params->inPublic.publicArea.type = TPM2_ALG_KEYEDHASH;
+
+    FILE *out = open_csv("TPM2_Create_KEYEDHASH.csv",
+            "scheme;details;duration_mean;error_codes", "w");
+    FILE *out_all = open_csv("TPM2_Create_KEYEDHASH_all.csv",
+            "scheme;details;duration;return_code", "w");
+
+    for (int i = 0; i < sizeof(schemes) / sizeof(TPMI_ALG_KEYEDHASH_SCHEME); ++i) {
+        TPMI_ALG_KEYEDHASH_SCHEME scheme = schemes[i];
+        TPMU_PUBLIC_PARMS parameters;
+        parameters.keyedHashDetail.scheme.scheme = scheme;
+
+        switch (scheme) {
+        case TPM2_ALG_HMAC:
+            /* HMAC only signs, doesn't decrypt */
+            params->inPublic.publicArea.objectAttributes &= ~TPMA_OBJECT_DECRYPT;
+            params->inPublic.publicArea.objectAttributes |= TPMA_OBJECT_SIGN_ENCRYPT;
+            for (int hashAlg = TPM2_ALG_FIRST; hashAlg <= TPM2_ALG_LAST; ++hashAlg) {
+                parameters.keyedHashDetail.scheme.details.hmac.hashAlg = hashAlg;
+                params->inPublic.publicArea.parameters = parameters;
+                char param_fields[16];
+                snprintf(param_fields, 16, "%04x;%04x", scheme, hashAlg);
+                test_and_measure(sapi_context, param_fields, params,
+                        parentHandle, out, out_all);
+            }
+            params->inPublic.publicArea.objectAttributes |= TPMA_OBJECT_DECRYPT;
+            params->inPublic.publicArea.objectAttributes &= ~TPMA_OBJECT_SIGN_ENCRYPT;
+            break;
+        case TPM2_ALG_XOR:
+            for (int hashAlg = TPM2_ALG_FIRST; hashAlg <= TPM2_ALG_LAST; ++hashAlg) {
+                for (int kdf = TPM2_ALG_FIRST; kdf <= TPM2_ALG_LAST; ++kdf) {
+                    TPMS_SCHEME_XOR details = {
+                        .hashAlg = hashAlg,
+                        .kdf = kdf
+                    };
+                    parameters.keyedHashDetail.scheme.details.exclusiveOr = details;
+                    params->inPublic.publicArea.parameters = parameters;
+                    char param_fields[16];
+                    snprintf(param_fields, 16, "%04x;%04x,%04x", scheme, hashAlg, kdf);
+                    test_and_measure(sapi_context, param_fields, params,
+                            parentHandle, out, out_all);
+                }
+            }
+            break;
+        case TPM2_ALG_NULL:
+            {
+                char param_fields[16];
+                snprintf(param_fields, 16, "%04x;", scheme);
+                test_and_measure(sapi_context, param_fields, params,
+                        parentHandle, out, out_all);
+            }
+            break;
+        default:
+            fprintf(stderr, "CreatePrimary: unknown keyedhash scheme");
+        }
+    }
+
+    fclose(out);
+    fclose(out_all);
+}
+
 void measure_Create(TSS2_SYS_CONTEXT *sapi_context)
 {
     printf("TPM2_Create:\n");
@@ -162,9 +309,9 @@ void measure_Create(TSS2_SYS_CONTEXT *sapi_context)
     prepare_create_params(&params);
 
     measure_Create_RSA(sapi_context, &params, parentHandle);
-    //measure_Create_ECC(sapi_context, &params, parentHandle);
-    //measure_Create_SYMCIPHER(sapi_context, &params, parentHandle);
-    //measure_Create_KEYEDHASH(sapi_context, &params, parentHandle);
+    measure_Create_ECC(sapi_context, &params, parentHandle);
+    measure_Create_SYMCIPHER(sapi_context, &params, parentHandle);
+    measure_Create_KEYEDHASH(sapi_context, &params, parentHandle);
 
     Tss2_Sys_FlushContext(sapi_context, parentHandle);
 }
