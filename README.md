d7e9be3b7dd531ca4b97c2458f946af8a83fb6e2
Simon Struk <simon.struk@gmail.com>
2019-03-08 09:33:41 +0100

    Update run_image.py.

--- README.md
+++ README.md
@@ -5,5 +5,9 @@ Tool for measuring TPM capabilities, generated primes etc.
 Based on [tpm2-tools](https://github.com/tpm2-software/tpm2-tools) project.
 
 #### How to use:
-* download a docker image: `docker image pull simonstruk/tpm2-algtest:v0.1`
-* run script: `./run_image.py`
+To test your TPM with `tpm2-algtest`, you need only to have `docker` and `python3`, download
+the `run_image.py` script and run the following commands. Docker priviledges are needed:
+* pull a docker image: `python3 -m, run_image pull`
+* run quicktest: `python3 -m run_image quicktest`
+* run keygen test: `python3 -m run_image keygen`
+* run keygen all tests: `python3 -m run_image fulltest`
--- run_image.py
+++ run_image.py
@@ -1,25 +1,41 @@
-#!/usr/bin/env python3
-
 import os
 import subprocess
 import zipfile
+import argparse
 
-def main():
+device = '/dev/tpm0'
+
+def zip():
+    zipf = zipfile.ZipFile('out.zip', 'w', zipfile.ZIP_DEFLATED)
+    for file in os.listdir('out'):
+        zipf.write('out/' + file)
+
+def quicktest():
     os.makedirs('out', exist_ok=True)
-    device = '/dev/tpm0'
+    entrypoint = 'tpm2_getcap'
+    image_args = '-c algorithms'
+    run_image = ['docker', 'run', '-it', '--init', '--device=' + device,
+            '--entrypoint=tpm2_getcap' , 'simonstruk/tpm2-algtest:v0.1']
+    with open('out/Quicktest_algorithms.txt', 'w') as outfile:
+        subprocess.run(run_image + ['-c', 'algorithms'], stdout=outfile).check_returncode()
+    with open('out/Quicktest_commands.txt', 'w') as outfile:
+        subprocess.run(run_image + ['-c', 'commands'], stdout=outfile).check_returncode()
+    with open('out/Quicktest_properties-fixed.txt', 'w') as outfile:
+        subprocess.run(run_image + ['-c', 'properties-fixed'], stdout=outfile).check_returncode()
+    with open('out/Quicktest_properties-variable.txt', 'w') as outfile:
+        subprocess.run(run_image + ['-c', 'properties-variable'], stdout=outfile).check_returncode()
+    with open('out/Quicktest_ecc-curves.txt', 'w') as outfile:
+        subprocess.run(run_image + ['-c', 'ecc-curves'], stdout=outfile).check_returncode()
+    with open('out/Quicktest_handles-persistent.txt', 'w') as outfile:
+        subprocess.run(run_image + ['-c', 'handles-persistent'], stdout=outfile).check_returncode()
 
-    # TODO: maybe let user pull the image
-    # TODO: use docker module
-    #subprocess.run([
-        #'docker', 'image', 'pull', 'simonstruk/tpm2-algtest:v0.1'
-        #]).check_returncode()
-
-    run_algtest = [ 'docker', 'run', '-it', '--init', '--device=' + device,
+def keygen():
+    run_image = [ 'docker', 'run', '-it', '--init', '--device=' + device,
             '--volume=' + os.getcwd() + '/out:/tpm2-algtest/build/out:z',
             'simonstruk/tpm2-algtest:v0.1' ]
-
+    os.makedirs('out', exist_ok=True)
     for keylen in 1024, 2048:
-        subprocess.run(run_algtest + [
+        subprocess.run(run_image + [
             '-T', 'device',
             '-s', 'keygen',
             '-t', 'rsa',
@@ -28,11 +44,31 @@ def main():
             '--exportkeys'
             ]).check_returncode()
 
-    # TODO: flush context after use or error (option)
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('command', metavar='command', type=str)
+    args = parser.parse_args()
 
-    zipf = zipfile.ZipFile('out.zip', 'w', zipfile.ZIP_DEFLATED)
-    for file in os.listdir('out'):
-        zipf.write('out/' + file)
+    if args.command == 'pull':
+        subprocess.run([
+            'docker', 'image', 'pull', 'simonstruk/tpm2-algtest:v0.1'
+            ]).check_returncode()
+
+    if args.command == 'quicktest':
+        quicktest()
+        zip()
+    elif args.command == 'keygen':
+        keygen()
+        zip()
+    elif args.command == 'fulltest':
+        quicktest()
+        keygen()
+        zip()
+
+    else:
+        print('invalid command')
+
+    # TODO: flush context after use or error (option)
 
 if __name__ == '__main__':
     main()
