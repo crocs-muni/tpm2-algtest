cbb4b09ba2a6dd2105f4fd798bbe271065aa4522
Daniel Zatovic <daniel.zatovic@gmail.com>
2020-02-18 23:01:29 +0100

    fix percentage calculation

--- lib/status.c
+++ lib/status.c
@@ -1,10 +1,10 @@
 #include "status.h"
 
 unsigned get_progress_percentage(struct progress *prog) {
-	return (double) prog->current / prog->total;
+	return ((double) prog->current / prog->total) * 100;
 }
 
 unsigned inc_and_get_progress_percentage(struct progress *prog) {
 	prog->current++;
-	return (double) prog->current / prog->total;
+	return get_progress_percentage(prog);
 }
